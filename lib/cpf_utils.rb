bf9319b4364980a514662ffcf8b27996e69ed0b4
Jackson Pires <jackson.pires@gmail.com>
2014-03-23 19:05:17 -0300

    Refatorando/Adicionando a classe CPF.

--- lib/cpf_utils.rb
+++ lib/cpf_utils.rb
@@ -1,8 +1,8 @@
 require "cpf_utils/version"
 require "cpf_utils/string"
+require "cpf_utils/cpf"
 
 module CpfUtils
-
 
   # Gera um número de CPF.
   #
@@ -30,70 +30,10 @@ module CpfUtils
     cpf_number.to_s.valid_cpf?
   end
 
+  private
+
   # Sorteia nove números para compor o um novo CPF
   def self.sample_numbers
     [*0..9].sample(9)
-  end
-
-
-  class Cpf
-
-    # Acessor de leitura para os números
-    attr_reader :numbers
-
-    # Inicializador da classe
-    def initialize(numbers)
-      if numbers.is_a? String
-        numbers = numbers.split('')
-      elsif numbers.is_a? Fixnum
-        numbers = numbers.to_s.split('')
-      end
-
-      @numbers = numbers
-    end
-
-    # Gera o CPF propriamente dito
-    def generate_cpf
-      @numbers << first_digit
-      @numbers << second_digit
-      @numbers.join("")
-    end
-
-    # Gera o primeiro dígito verificador
-    def first_digit
-      if @numbers.size == 9
-        value = 0
-        multipliers = [10,9,8,7,6,5,4,3,2]
-
-        multipliers.each_with_index do |mult, index|
-          value += @numbers[index].to_i * mult
-        end
-
-        check_remainder(value % 11)
-      end
-    end
-
-    # Gera o segundo dígito verificador
-    def second_digit
-      if @numbers.size == 10
-        value = 0
-        multipliers = [11,10,9,8,7,6,5,4,3,2]
-
-        multipliers.each_with_index do |mult, index|
-          value += @numbers[index].to_i * mult
-        end
-
-        check_remainder(value % 11)
-      end
-    end
-
-    # Checa o resto da divisão
-    def check_remainder(remainder)
-      if remainder < 2
-        0
-      else
-        (11 - remainder)
-      end
-    end
   end
 end
---
+++ lib/cpf_utils/cpf.rb
@@ -0,0 +1,62 @@
+module CpfUtils
+  class Cpf
+
+    # Acessor de leitura para os números
+    attr_reader :numbers
+
+    # Inicializador da classe
+    def initialize(numbers)
+      if numbers.is_a? String
+        numbers = numbers.split('')
+      elsif numbers.is_a? Fixnum
+        numbers = numbers.to_s.split('')
+      end
+
+      @numbers = numbers
+    end
+
+    # Gera o CPF propriamente dito
+    def generate_cpf
+      @numbers << first_digit
+      @numbers << second_digit
+      @numbers.join("")
+    end
+
+    # Gera o primeiro dígito verificador
+    def first_digit
+      if @numbers.size == 9
+        value = 0
+        multipliers = [10,9,8,7,6,5,4,3,2]
+
+        multipliers.each_with_index do |mult, index|
+          value += @numbers[index].to_i * mult
+        end
+
+        check_remainder(value % 11)
+      end
+    end
+
+    # Gera o segundo dígito verificador
+    def second_digit
+      if @numbers.size == 10
+        value = 0
+        multipliers = [11,10,9,8,7,6,5,4,3,2]
+
+        multipliers.each_with_index do |mult, index|
+          value += @numbers[index].to_i * mult
+        end
+
+        check_remainder(value % 11)
+      end
+    end
+
+    # Checa o resto da divisão
+    def check_remainder(remainder)
+      if remainder < 2
+        0
+      else
+        (11 - remainder)
+      end
+    end
+  end
+end
