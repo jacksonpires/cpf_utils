6f6bb75d00144e6a62db46a88f48b915fe777a3b
Jackson Pires <jackson.pires@gmail.com>
2014-03-23 20:24:51 -0300

    Novos testes para os métodos em portugês.

--- spec/cpf_utils/string_spec.rb
+++ spec/cpf_utils/string_spec.rb
@@ -6,10 +6,21 @@ describe String do
     expect("12345678912".valid_cpf_mask?).to be_true
   end
 
+  it "#mascara_de_cpf_valida?" do
+    expect("123.456.789-12".mascara_de_cpf_valida?).to be_true
+    expect("12345678912".mascara_de_cpf_valida?).to be_true
+  end
+
   it "#valid_cpf_mask? - be false" do
     expect("123..456.789-12".valid_cpf_mask?).to be_false
     expect("123.456.789--12".valid_cpf_mask?).to be_false
     expect("123.456.789-1E".valid_cpf_mask?).to be_false
+  end
+
+  it "#mascara_de_cpf_valida? - be false" do
+    expect("123..456.789-12".mascara_de_cpf_valida?).to be_false
+    expect("123.456.789--12".mascara_de_cpf_valida?).to be_false
+    expect("123.456.789-1E".mascara_de_cpf_valida?).to be_false
   end
 
   it "#valid_cpf?" do
@@ -17,8 +28,48 @@ describe String do
     expect(cpf.valid_cpf?).to be_true
   end
 
+  it "#cpf_valido?" do
+    cpf = CpfUtils.cpf
+    expect(cpf.cpf_valido?).to be_true
+  end
+
   it "#to_cpf_format?" do
     cpf = CpfUtils.cpf
     expect(cpf.to_cpf_format).to match(/^[+-]?[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}+$/)
+  end
+
+  it "#para_formato_cpf?" do
+    cpf = CpfUtils.cpf
+    expect(cpf.para_formato_cpf).to match(/^[+-]?[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}+$/)
+  end
+
+  it "#generate_cpf" do
+    valid_cpf = CpfUtils.cpf
+    final_cpf = valid_cpf[0..8].generate_cpf
+
+    expect(final_cpf).to be_a_kind_of(String)
+    expect(final_cpf.length).to eql(11)
+  end
+
+  it "#gerar_cpf" do
+    valid_cpf = CpfUtils.cpf
+    final_cpf = valid_cpf[0..8].gerar_cpf
+
+    expect(final_cpf).to be_a_kind_of(String)
+    expect(final_cpf.length).to eql(11)
+  end
+
+  it "#gerar_cpf_formatado" do
+    valid_cpf = CpfUtils.cpf
+    final_cpf = valid_cpf[0..8].gerar_cpf_formatado
+
+    expect(final_cpf).to match(/^[+-]?[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}+$/)
+  end
+
+  it "#generate_cpf_formatted" do
+    valid_cpf = CpfUtils.cpf
+    final_cpf = valid_cpf[0..8].generate_cpf_formatted
+
+    expect(final_cpf).to match(/^[+-]?[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}+$/)
   end
 end
--- spec/cpf_utils_spec.rb
+++ spec/cpf_utils_spec.rb
@@ -2,43 +2,40 @@ require "spec_helper"
 
 describe CpfUtils do
 
-  context 'CpfUtils' do
+  it ".cpf" do
+    valid_cpf = CpfUtils.cpf
+    expect(valid_cpf).to be_a_kind_of(String)
+    expect(valid_cpf.length).to eql(11)
+  end
 
-    it ".cpf" do
-      valid_cpf = CpfUtils.cpf
-      expect(valid_cpf).to be_a_kind_of(String)
-      expect(valid_cpf.length).to eql(11)
-    end
+  it ".cpf_formatted" do
+    valid_cpf = CpfUtils.cpf_formatted
+    expect(valid_cpf).to match(/^[+-]?[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}+$/)
+  end
 
-    it ".cpf_formatted" do
-      valid_cpf = CpfUtils.cpf_formatted
-      expect(valid_cpf).to match(/^[+-]?[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}+$/)
-    end
+  it ".cpf_formatado" do
+    valid_cpf = CpfUtils.cpf_formatado
+    expect(valid_cpf).to match(/^[+-]?[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}+$/)
+  end
 
-    it ".cpf_formatado" do
-      valid_cpf = CpfUtils.cpf_formatado
-      expect(valid_cpf).to match(/^[+-]?[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}+$/)
-    end
+  it ".valid_cpf?" do
+    valid_cpf = CpfUtils.cpf
+    expect(CpfUtils.valid_cpf? valid_cpf).to be_true
+  end
 
-    it ".valid_cpf?" do
-      valid_cpf = CpfUtils.cpf
-      expect(CpfUtils.valid_cpf? valid_cpf).to be_true
-    end
+  it ".cpf_valido?" do
+    cpf_valido = CpfUtils.cpf
+    expect(CpfUtils.cpf_valido? cpf_valido).to be_true
+  end
 
-    it ".cpf_valido?" do
-      cpf_valido = CpfUtils.cpf
-      expect(CpfUtils.cpf_valido? cpf_valido).to be_true
-    end
+  it ".valid_cpf? - be false" do
+    invalid_cpf = "123.456.789-12"
+    expect(CpfUtils.valid_cpf? invalid_cpf).to be_false
+  end
 
-    it ".valid_cpf? - be false" do
-      invalid_cpf = "123.456.789-12"
-      expect(CpfUtils.valid_cpf? invalid_cpf).to be_false
-    end
-
-    it ".sample_numbers" do
-      numbers = CpfUtils.sample_numbers
-      expect(numbers.size).to eql(9)
-      expect(numbers).to be_a_kind_of(Array)
-    end
+  it ".sample_numbers" do
+    numbers = CpfUtils.sample_numbers
+    expect(numbers.size).to eql(9)
+    expect(numbers).to be_a_kind_of(Array)
   end
 end
