8f6cf7927e9c02c4c5611ac4baad50898689c90f
Jackson Pires <jackson.pires@gmail.com>
2014-03-27 14:41:27 -0300

    Corrigindo spec_helper para o CodeClimate.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
-require "cpf_utils"
-require_relative "../lib/cpf_utils/cpf"
+# Require Code Climate Test Reporter
 require "codeclimate-test-reporter"
 
 # Starts Code Climate Test Reporter
 CodeClimate::TestReporter.start
+
+# Require other files of project
+require "cpf_utils"
+require_relative "../lib/cpf_utils/cpf"
